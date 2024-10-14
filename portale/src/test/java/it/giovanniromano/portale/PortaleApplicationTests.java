0eb00bd5571207aabd2c3f64307e19266ed4dfc6
Giovanni <giovanniromano1998@icloud.com>
2024-10-14 18:40:30 +0200

    Creazione portale

---
+++ portale/src/test/java/it/giovanniromano/portale/PortaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giovanniromano.portale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
